87c76557ef9102c3ad45008a054f85141177daa7
Yujiro <siroidanngo1061@yahoo.co.jp>
2023-02-03 15:31:37 +0000

    課題提出のため

---
+++ PHP-Laravel-12/web.php
@@ -0,0 +1,49 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+use App\Http\Controllers\Admin\NewsController;
+Route::controller(NewsController::class)
+->prefix('admin')->group(function() {
+    Route::get('news/create', 'add')->middleware('auth');
+});
+
+//PHP-Laravel-09 課題3   
+//Route::controller(AAAController::class)
+//->group(function() {
+    //Route::get('XXX' 'bbb');
+//});
+
+//PHP-Laravel-09 課題4
+use App\Http\Controllers\Admin\ProfileController;
+Route::controller(ProfileController::class)
+->prefix('admin')->group(function() {
+    Route::get('profile/create', 'add')->middleware('auth');
+    Route::get('profile/edit', 'edit')->middleware('auth');
+});
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
